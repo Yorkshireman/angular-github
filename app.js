76d315582c2cc037964705a1d25ddc17526ec4c2
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-10 18:12:03 +0100

    set port to 8080 locally and for heroku

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/');
@@ -13,7 +15,7 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
