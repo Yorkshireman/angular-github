e5f121485f17325260d5c0d14015862defe465d2
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-11 11:54:11 +0100

    make env variable global

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -1,6 +1,5 @@
 githubUserSearch.factory('Search', ['$http', function($http) {
   var queryUrl = 'https://api.github.com/search/users';
-  var githubToken = process.env.GITHUB_TOKEN
   return {
     query: function(searchTerm) {
       return $http({
