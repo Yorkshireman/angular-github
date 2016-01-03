a19e7c63ed5574eb18b16d5051e49414ecd22606
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-03 23:47:20 +0000

    githubToken variable problem fixed

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -2,11 +2,10 @@ githubUserSearch.controller('GitUserSearchController', ['$resource', function($r
   var self = this;
 
   var searchResource = $resource('https://api.github.com/search/users/');
-  var githubToken = githubToken;
 
   self.doSearch = function() {
     self.searchResult = searchResource.get(
-      { q: self.searchTerm, access_token: githubToken }
+      { q: self.searchTerm, access_token: githubTokenFromSecretsJs }
     );
   };
 }]);
